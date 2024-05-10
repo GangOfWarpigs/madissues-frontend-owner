99b96a5b7c799f9d3a946f2ce8b7a991a659a1a7
Sara González Ramírez <saragonza.lez0608@gmail.com>
2024-05-10 22:41:15 +0100

    fixed scroll

--- src/pages/base/projects/pages/projectsDetailed/pages/ProjectsConfiguration.vue
+++ src/pages/base/projects/pages/projectsDetailed/pages/ProjectsConfiguration.vue
@@ -9,7 +9,7 @@
 
 <template>
   <div class="w-full min-h-screen flex pl-[300px]">
-    <div class="w-1/4 border-r-2 py-10 px-5">
+    <div class="w-[400px] border-r-2 py-10 px-5 h-screen fixed">
       <nav class="w-full flex flex-col">
         <ul class="w-full flex flex-col space-y-5">
           <li class="flex">
@@ -51,7 +51,7 @@
         </ul>
       </nav>
     </div>
-    <div class="w-full flex flex-col px-20 py-10">
+    <div class="w-full flex flex-col px-20 py-10 box-border ml-[400px]">
       <router-view></router-view>
     </div>
   </div>  
